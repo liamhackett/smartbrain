d4c08b10dbc5e2255baedc339f126e9601aa2800
Liam Hackett <ljh1oo@yahoo.com>
2023-09-13 19:34:18 -0400

    Changed some styling

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,10 +7,10 @@ const ImageLinkForm = () => {
         <div className="form">
             <div className="text-container">
                 <h3 className="f3 text">
-                    {'This Brain will detect faces in your pictures. Give it a try!'}
+                    Smart Brain will detect faces in your pictures. Give it a try!
                 </h3>
             </div>
-            <div className="input-container">
+            <div className="input-container shadow-2">
                 <Form.Control size="lg" type="text" placeholder="Image Link" className="focus"/>
                 <Button variant="custom" size="lg">Detect</Button>
             </div> 
