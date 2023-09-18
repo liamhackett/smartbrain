f3c89ec4b0db5c6d39303de934bc687f746196db
Liam Hackett <ljh1oo@yahoo.com>
2023-09-18 14:14:14 -0400

    Fixed spinner

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,58 +1,45 @@
-// import React from "react";
-// import "./FaceRecognition.css";
-
-// const FaceRecognition = ({ boxes, imageUrl, isFile, format, celebrity, celebNames, loading }) => {
-//   return (
-//     <div className="center ma">
-//       <div className="relative mt2">
-//         {isFile ? (
-//           <img className="shadow-2" id="inputImage" src={`data:${format};base64,${imageUrl}`} alt="" />
-//         ) : (
-//           <img className="shadow-2" id="inputImage" src={imageUrl} alt="" />
-//         )}
-       
-//         {boxes.map((box, index) => (
-//           <div
-//             key={index}
-//             className="bounding-box"
-//             style={{
-//               top: box.topRow,
-//               right: box.rightCol,
-//               bottom: box.bottomRow,
-//               left: box.leftCol,
-//             }}
-//           >
-//             {celebrity && celebNames[index] && (
-//               <div className="celeb-name">
-//                 <mark className="highlight">{celebNames[index]}</mark>
-//               </div>
-//             )}
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default FaceRecognition;
-
-import React from "react";
+import React, { Component } from "react";
 import "./FaceRecognition.css";
-import Spinner from 'react-bootstrap/Spinner';
+import { Spinner } from "react-bootstrap";
+class FaceRecognition extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: "none"
+    };
+  }
+  
+  render() {
+    const { boxes, imageUrl, isFile, format, celebrity, celebNames, loading } = this.props;
 
-const FaceRecognition = ({ boxes, imageUrl, isFile, format, celebrity, celebNames, loading }) => {
-  return (
-    <div className="center ma">
-      {loading ? (
-        <Spinner className="custom-spinner" animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
-      ) : (
+    return (
+      <div className="center ma">  
         <div className="relative mt2">
+          <div className="custom-spinner-container">
+          {
+            loading && (
+              <Spinner className="custom-spinner" animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            )
+          }
+          </div>
           {isFile ? (
-            <img className="shadow-2" id="inputImage" src={`data:${format};base64,${imageUrl}`} alt="" />
+            <img
+              className="shadow-2"
+              id="inputImage"
+              src={`data:${format};base64,${imageUrl}`}
+              alt=""
+              style={{ visibility: loading ? "hidden" : "" }}
+            />
           ) : (
-            <img className="shadow-2" id="inputImage" src={imageUrl} alt="" />
+            <img
+              className="shadow-2"
+              id="inputImage"
+              src={imageUrl}
+              alt=""
+              style={{ visibility: loading ? "hidden" : "" }}
+            />
           )}
 
           {boxes.map((box, index) => (
@@ -71,13 +58,14 @@ const FaceRecognition = ({ boxes, imageUrl, isFile, format, celebrity, celebName
                   <mark className="highlight">{celebNames[index]}</mark>
                 </div>
               )}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+          </div>
+        ))}
+      </div>
+
+
+      </div>
+    );
+  }
 };
 
 export default FaceRecognition;
-
